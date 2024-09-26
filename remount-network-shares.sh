4c2b7d7ec31d981d57d34b4f6b7f733c55a83d2f
Maxwell Luong <maxwell.luong@quaintengineer.com>
2024-09-25 23:27:10 -0700

    +x file permission

--- remount-network-shares.sh
+++ remount-network-shares.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
 . .env
-umount /mnt/pve/$SMB_SHARE
-pvesm set $SMB_SHARE --disable
-pvesm set $SMB_SHARE --disable 0
+
+for share in "${CIFS_SHARES[@]}"; do
+  echo "unmounting $share"	
+  umount /mnt/pve/$share || true
+  pvesm set $share --disable
+  pvesm set $share --disable 0
+  echo "re-mounted $share"
+done
+echo "done."
